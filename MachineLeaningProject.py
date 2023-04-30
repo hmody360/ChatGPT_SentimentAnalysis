06ffc9449359b9a1dd023d52f84ca785db1c5b72
hmody360 <s441010207@st.uqu.edu.sa>
2023-04-30 13:59:35 +0300

    added model selection

--- MachineLeaningProject.py
+++ MachineLeaningProject.py
@@ -4,6 +4,18 @@
 import pandas as pd
 import numpy as np
 import re
+#importing data visualization modules
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
+from PIL import Image
+# importing Scikit learn models for classification
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import jaccard_score, accuracy_score, f1_score, classification_report
+from sklearn.feature_extraction.text import CountVectorizer
 # Creating a data frame using the CSV file containing all the gathered tweets using its path
 df = pd.read_csv("C:/Users/Owner/Desktop/MachineLeaningFile/file.csv")
 
@@ -32,7 +44,7 @@ df.head()
 text = [i[0] for i in df.tweet_list]
 df['text'] = text
 df = df[['text', 'labels']]
-df.head
+df.head()
 
 # Removing all characters that aren't a number or in the alphabet from the text
 # setting up regex
@@ -57,4 +69,71 @@ for i in trim_list:
 
 df['text'] = trim_list
 
-df.head
+df.head()
+# -----------------------Data Visualization---------------------------------
+# #initializing comment and stop words variables
+# comment_words = ""
+# stopwords = set(STOPWORDS)
+
+# #method to get each word seperately
+# for val in df.text:
+#     val = str(val)
+#     tokens = val.split()
+
+#     for i in range(len(tokens)):
+#         tokens[i] = tokens[i].lower()
+
+#     comment_words += " ".join(tokens)+" "
+
+# #setting up mask to visualize data on
+# mask = np.array(Image.open("C:/Users/Owner/Desktop/MachineLeaningFile/masks/comment.png"))
+
+# #setting up the wordcloud
+# wordcloud = WordCloud(width = 800, height = 800,
+#             background_color ='pink', 
+#             stopwords= stopwords,
+#             min_font_size = 10, mask = mask).generate(comment_words)
+# #plotting the WordCloud Image
+# plt.figure(figsize=(8,8), facecolor = None)
+# plt.imshow(wordcloud)
+# plt.axis("off")
+# plt.tight_layout(pad = 0)
+
+# plt.show()
+# -----------------------Model Selection---------------------------------
+#labels to integers guide
+# 1 = Good
+# 0 = netural
+#-1 = bad
+df['lab_int'] = np.where(df['labels']=='good',1,np.where(df['labels']=='bad',-1,0))
+#creating train and test variables
+X_train,X_test,y_train,y_test = train_test_split(df['text'],df['lab_int'],test_size=0.3,random_state=1)
+#converting the text document into a matrix of tokens
+vec = CountVectorizer(ngram_range=(1,3),stop_words="english")
+#fitting the vectorized sets into the variables
+X_train = vec.fit_transform(X_train)
+X_test = vec.transform(X_test)
+#Applying Navie Bayas Model
+from sklearn.naive_bayes import MultinomialNB
+
+nb = MultinomialNB()
+nb.fit(X_train,y_train)
+
+preds = nb.predict(X_test)
+print(classification_report(y_test,preds))
+
+# using logistic regression method
+log = LogisticRegression()
+log.fit(X_train,y_train)
+
+preds = log.predict(X_test)
+print(classification_report(y_test,preds))
+# -----------------------Hyper Parameter Tuning---------------------------------
+from sklearn.model_selection import GridSearchCV,RepeatedStratifiedKFold
+
+param_grid = {"alpha":[0.1,0,1.0,10,100]}
+
+grid_search = GridSearchCV(MultinomialNB(), param_grid,verbose=2)
+grid_search.fit(X_train,y_train)
+
+grid_search.best_params_
